c6d2ca807ef00e113db3cc5d28047cfc98e7f574
vigneshreddy <vrtarimala@gmail.com>
2022-11-14 22:17:11 +0530

    1.0.1

---
+++ src/main/java/com/example/demo/model/Cart_items.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Cart_items")
+public class Cart_items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    private item ordered_item;
+
+    @Column(name = "Quantity")
+    private int quantity;
+
+    public Cart_items(item ordered_item, int quantity) {
+        this.ordered_item = ordered_item;
+        this.quantity = quantity;
+    }
+
+    public item getOrdered_item() {
+        return ordered_item;
+    }
+
+    public void setOrdered_item(item ordered_item) {
+        this.ordered_item = ordered_item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
